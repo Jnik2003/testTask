6353fdaef903c75cc471ee825f49bf1fa051b2ee
Georgii <kaunas16@gmail.com>
2022-11-13 18:39:08 +0300

    feat: start&end test task

---
+++ App.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <div class="box-1">
+          <tags-box :tags="tags" :align="alignLeft" />
+        </div>
+        <div class="box-2">
+          <tags-box :tags="tags" :align="alignCenter" />
+        </div>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import TagsBox from "@/components/TagsBox";
+export default {
+  name: "App",
+
+  components: { TagsBox },
+  data() {
+    return {
+      tags: {
+        rating: {
+          icon: "mdi-star",
+          delimeter: "",
+          value: "5",
+        },
+        type: {
+          icon: "",
+          delimeter: "mdi-circle-medium",
+          value: "Обзорная",
+        },
+        transport: {
+          icon: "mdi-bus",
+          delimeter: "mdi-circle-medium",
+          value: "На автобусе",
+        },
+        date: {
+          icon: "",
+          delimeter: "mdi-circle-medium",
+          value: "25 марта",
+        },
+        time: {
+          icon: "mdi-clock-outline",
+          delimeter: "mdi-circle-medium",
+          value: "6 часов",
+        },
+      },
+      alignLeft: "flex-start",
+      alignCenter: "center",
+      
+    };
+  },
+};
+</script>
+<style>
+body, html{
+  box-sizing: border-box;
+}
+.box-1{
+  border: 1px solid black;
+  margin-bottom: 20px;
+  display: flex;
+  /* justify-content: flex-start; */
+  overflow-y: hidden;
+}
+.box-2{
+  margin-left: 100px;
+  border: 1px solid black;
+}
+
+</style>
---
+++ components/TagsBox.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="tag-box" :class="alignFlex">
+    <tag-single v-for="(tag, ind) in tags" :key="ind" :tag="tag" />
+  </div>
+</template>
+
+<script>
+import TagSingle from "@/components/TagSingle";
+export default {
+  name: "TagsBox",
+  components: { TagSingle },
+  props: {
+    tags: {
+      type: Object,
+      default: () => {},
+    },
+    align: {
+      type: String,
+      default: "left",
+    },
+  },
+  computed: {
+    alignFlex() {
+      return {
+        alignLeft: this.align === "left",
+        alignCenter: this.align === "center",
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.tag-box {
+  display: flex;
+  gap: 10px;
+  flex-wrap: wrap;
+  overflow: hidden;
+  height: 32px;
+}
+
+.alignCenter {
+  justify-content: center;
+}
+</style>
